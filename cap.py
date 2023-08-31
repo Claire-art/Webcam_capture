9a262117f5e41b4b38a1dbe5372d32c60fcd9bfe
HAN JUA <69968801+Claire-art@users.noreply.github.com>
2023-08-31 14:15:48 +0900

    Update cap.py

--- cap.py
+++ cap.py
@@ -2,19 +2,25 @@ import cv2
 import os
 import time
 
-cap = cv2.VideoCapture(0)
+
 
 # 촬영 간격 설정
 capture_interval = 0.1
 
 # 저장할 폴더 설정
-output_folder = "captured_images"
+output_folder = "/home/jua/daon/custom/ghana_2"
 os.makedirs(output_folder, exist_ok=True)
 capture_count = 0
 
+cap = cv2.VideoCapture(2)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH,480)  #해상도 조절해주기,웹캠사용시 필요
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT,640)
+
 while True:
     ret, frame = cap.read()
+    cv2.imshow("VideoFrame", frame)
     filename = os.path.join(output_folder, f"{time.time():.6f}.jpg")
+
 
     cv2.imwrite(filename, frame)
     capture_count += 1
